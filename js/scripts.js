e2e1af12cfd6e693f5d5b40760a47a897a6489e0
Nathan Conn <nconn34@gmail.com>
2021-10-27 19:43:56 -0700

    Added jQuery functions.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const jquery360 = require("./jquery-3.6.0");
+
+jquery360("h1").click(function(){
+  alert("This is a heading.");
+});
+
+jQuery("p").click(function(){
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function(){
+  alert("This is an image.");
+});
